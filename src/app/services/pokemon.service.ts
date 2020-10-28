571836fdbb9f8193e156f3a66b72c1a7b93d3425
Carlos Eduardo Artioli <caduartioli@gmail.com>
2020-10-28 18:21:29 -0300

    Utilizando API da Pokedex

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  public buscarPokemons(url: string) {
+    return this.http.get(url);
+  }
+
+  public buscarPokemonId(url: string) {
+    return this.http.get(`${url}`);
+  }
+}
